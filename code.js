4e8639998fa3f50176cc0c90c59b16265848701f
Andrew <andrewvt97@gmail.com>
2023-03-20 12:19:10 -0400

    Add CSS stylesheet to keep styling separate

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
-     
+
 
 const idNames = ["british-currency","canadian-currency","european-currency","indian-currency","australian-currency","japanese-currency","chinese-currency","russian-currency"] 
 const numofCountries=8; // number of countries
@@ -9,13 +9,17 @@ const currencyTokens = ["£","$","€","₹","$","¥","¥","₽"]; // currency t
 function convert(){
 //variables
 	const moneyToConvert =document.getElementById("american-dollar").value;
+	if (moneyToConvert==="")
+	{
+		moneyToConvert=0;
+	}
 	const americanDollar=parseFloat(moneyToConvert); // need to convert variables from string to number
 
 	const totals = []; // array to store the totals
 
 	// loop to calculate the totals
 	for (let i=0; i<numofCountries; i++){
-		totals.push(Math.round(americanDollar*exchangeRate[i]*100)/100);
+		totals.push((Math.round(americanDollar*exchangeRate[i]*100)/100).toFixed(2));
 	}
 
 	// loop to display the totals
